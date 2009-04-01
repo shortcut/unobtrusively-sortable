2d9761f806a6f50db45cd0ef95d328543489aa8c
August Lilleaas <augustlilleaas@gmail.com>
2009-04-01 10:18:54 +0200

    Using none-printing helpers.

--- lib/unobtrusively_sortable/helpers.rb
+++ lib/unobtrusively_sortable/helpers.rb
@@ -1,21 +1,20 @@
-# coding:utf-8 
 module UnobtrusivelySortable
   module Helpers
     # Renders the <ul>-list that contains the sortable elements. 'Elements' is
     # an array of ActiveRecord instances.
-    def unobtrusively_sortable_list(elements, options = {})
+    def unobtrusively_sortable_list(elements, options = {}, &block)
       options[:url] ||= !elements.empty? && __send__([:sort, elements.first.class.name.tableize, :path].join("_"))
-      
-      content_tag(:ul, elements.map {|e|
-        content_tag(:li, sortable_handles(e, options) + yield(e), :id => dom_id(e))
-      }.join, :class => "unobtrusively_sortable_list", :sort_url => options[:url])
+
+      concat(%{<ul class="unobtrusively_sortable_list" sort_url="#{options[:url]}">})
+      elements.each {|e| concat(content_tag(:li, sortable_handles(e, options) + capture(e, &block), :id => dom_id(e)))}
+      concat("</ul>")
     end
     
     def sortable_handles(e, options)
       returning("") do |out|
-        out << button_to("↑", options[:url] + "?direction=up&id=#{e.id}") unless e.first?
+        out << button_to("&uarr;", options[:url] + "?direction=up&id=#{e.id}") unless e.first?
         out << " "
-        out << button_to("↓", options[:url] + "?direction=down&id=#{e.id}") unless e.last?
+        out << button_to("&darr;", options[:url] + "?direction=down&id=#{e.id}") unless e.last?
         out << " "
       end
     end
